065f6716c84bed6495676605e78d8ec3dd432864
Inerjanuer <ibernate@zenware.com.co>
2019-10-10 10:14:31 -0500

    seguridad basica

---
+++ src/main/java/com/metodos/formales/WebAppInitializer.java
@@ -0,0 +1,22 @@
+package com.metodos.formales;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { WebSecConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { DemoApplication.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
---
+++ src/main/java/com/metodos/formales/WebSecInicializer.java
@@ -0,0 +1,7 @@
+package com.metodos.formales;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class WebSecInicializer extends AbstractSecurityWebApplicationInitializer{
+
+}
